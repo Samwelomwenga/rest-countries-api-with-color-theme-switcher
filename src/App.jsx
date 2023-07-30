8dfb9c501916c0120242b623785b00448812b464
SAMWEL OMWENGA <samwel.omwenga1@students.jkuat.ac.ke>
2023-07-30 18:01:54 +0300

    feat: add CountryDetailesPage routes
    Introduce CountryDetailesPage into the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import FetchCountries from "./api/FetchCountries";
 import Home from "./routes/Home";
+import CountryDetailesPage from "./routes/CountryDetailesPage";
 import { Route,Routes } from "react-router-dom";
 // import CountryDetailes from "./components/CountryDetailes"
 function App() {
@@ -46,6 +47,7 @@ function App() {
     <>
     <Routes>
       <Route path="/" element={<Home countries={countries} setCountries={setCountries}/>}/>
+      <Route path="/country" element={<CountryDetailesPage/>}/>
 
 
 
--- src/routes/CountryDetailesPage.jsx
+++ src/routes/CountryDetailesPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import CountryDetailes from '../components/CountryDetailes'
-const CountryDetailesPage = () => {
+const CountryDetailesPage = ({countries,setCountries}) => {
   return (
     <div>
         <Header/>
