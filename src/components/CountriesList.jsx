3327fb8d3a1b69adf24dc1efeb8965b05cb4112e
SAMWEL OMWENGA <samwel.omwenga1@students.jkuat.ac.ke>
2023-07-31 11:08:04 +0300

    fix: change the CountriesList component style
    change the CountriesList component style to align with the design

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -10,11 +10,11 @@ function CountriesList({countries}) {
       {countries.map((country) => (
         <div className=" bg-dark-blue mb-7 rounded-md mx-10 shadow-md " key={country?.name?.common}>
           <img className=" rounded-t-md" src={country.flags.svg} alt={country.name} />
-          <h2 className=" py-3 pl-5">{country.name.common}</h2>
+          <h2 className=" py-3 pl-5 font-extrabold">{country.name.common}</h2>
          <div className=" grid gap-0.5 pb-9 pl-5">
-         <p>Population:{' '}{country?.population}</p>
-          <p>Region:{' '}{country?.region}</p>
-          <p>Capital:{' '}{country?.capital}</p>
+         <p>Population:{' '}<span className=" text-sm text-very-light-grey">{country?.population}</span></p>
+          <p>Region:{' '}<span className=" text-sm text-very-light-grey">{country?.region}</span></p>
+          <p>Capital:{' '}<span className=" text-sm text-very-light-grey">{country?.capital}</span></p>
          </div>
         </div>
       ))}
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,9 +5,9 @@ import CountriesList from '../components/CountriesList'
 import Search from '../components/Search'
 export const Home = ({countries,setCountries}) => {
   return (
-    <main  className=' text-white'>
+    <main  className=' text-white bg-very-dark-blue-dm'>
       <Header/>
-        <section className=" bg-very-dark-blue-dm px-4 text-white">
+        <section className=" px-4">
     <Search countries={countries} setCountries={setCountries}/>
     <RegionFilter countries={countries} setCountries={setCountries}/>
     <CountriesList countries={countries}/>
