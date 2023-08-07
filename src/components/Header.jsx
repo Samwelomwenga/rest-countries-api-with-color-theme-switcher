298d4a542ad09b2fcbaab8b2afd256d9da7dfe50
SAMWEL OMWENGA <samwel.omwenga1@students.jkuat.ac.ke>
2023-08-08 00:07:25 +0300

    fix: change header padding Change the padding of the header to align it away from the edge of the screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { faMoon } from '@fortawesome/free-solid-svg-icons';
 function Header() {
   return (
     <>
-    <header className='flex justify-between bg-dark-blue w-full py-5'>
+    <header className='flex justify-between bg-dark-blue w-full py-5 px-4 md:px-10'>
         <h1 className=' font-extrabold'>Where is the world?</h1>
         <div className='flex gap-3'><FontAwesomeIcon icon={faMoon} size="lg" /><p>Dark Mode</p></div>
     </header>
