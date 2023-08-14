1915e9c3c6bc35f77f0c2e1a8b7cfbdf342c13b0
SAMWEL OMWENGA <samwel.omwenga1@students.jkuat.ac.ke>
2023-08-14 10:42:27 +0300

    fix: Debug  the borderlink component
    Debug the  borderlink component to prevent render undefined value

--- src/components/Borderlink.jsx
+++ src/components/Borderlink.jsx
@@ -1,33 +1,40 @@
 import { Link } from "react-router-dom";
 import Propstypes from "prop-types";
-import { useEffect,useState } from "react";
+import { useState,useEffect } from "react";
 export const Borderlink = ({ index, item, border }) => {
     const [borderNames, setBorderNames] = useState([]);
 
-    useEffect(()=>{
-        let ignore=false;
-        const fetchBorder = async (border) => {
-          const res = await fetch(
-            `https://restcountries.com/v3.1/alpha/${border}?fields=name`
-          );
-          const data = await res.json();
-          (!ignore) &&setBorderNames(data);
-        };
-        fetchBorder(border);
-        return () => {
-          ignore = true;
-        };
-      },[border]
-    );
+  useEffect(() => {
+    let ignore = false;
+    const fetchBorder = async () => {   
+      try {
+        const response = await fetch(
+          `https://restcountries.com/v3.1/alpha/${border}?fields=name`
+        );
+        const borderData = await response.json();
+        (!ignore)&& setBorderNames(borderData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchBorder();
+    return () => {
+      ignore = true;
+    };
+
+  }, [border]);
+
+
+
+
   return (
     <>
       <li
         key={index}
         className=" bg-white dark:bg-dark-blue py-0.5 px-1 text-center shadow-md rounded md:py-1 md:px-5 md:my-0 md:mx-1"
       >
-       {border&&( <Link to={`/${item.name.common}`}>{borderNames.name.common}</Link>)}
+       {border&&( <Link to={`/${item.name.common}`}>{borderNames.name&&borderNames.name.common}</Link>)}
       </li>
-      x
     </>
   );
 };
